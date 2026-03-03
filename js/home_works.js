7fa1b7b3569822c7ceb9cf2ace5915f98d9ca484
PAI0C4K1 <brevno766@gmail.com>
2026-03-03 18:07:35 +0600

    MABOY

--- js/home_works.js
+++ js/home_works.js
@@ -22,22 +22,77 @@ gmailButton.addEventListener("click", () => {
 
 //part B
 
-const parentBlock = document.querySelector(".parent_block");
-const childBlock = document.querySelector(".child_block");
+// const parentBlock = document.querySelector(".parent_block");
+// const childBlock = document.querySelector(".child_block");
 
-let position = 0;
+// let position = 0;
+// const speed = 2;
+
+// function moveBlock() {
+//     const parentWidth = parentBlock.clientWidth;
+//     const childWidth = childBlock.clientWidth;
+
+//     if (position <= parentWidth - childWidth) {
+//         childBlock.style.left = `${position}px`;
+//         position += speed;
+
+//         requestAnimationFrame(moveBlock);
+//     }
+// }
+
+// moveBlock();
+
+//lesson 1
+//move block V2
+const parentBlock = document.querySelector(".parent_block")
+const childBlock = document.querySelector(".child_block")
+
+let posX = 0, posY = 0;
+let disX = 1, disY = 0;
 const speed = 2;
 
-function moveBlock() {
-    const parentWidth = parentBlock.clientWidth;
-    const childWidth = childBlock.clientWidth;
+function move() {
+    const fixedParentWidth = parentBlock.clientWidth - childBlock.clientWidth;
+    const fixedParentHeight = parentBlock.clientHeight - childBlock.clientHeight;
 
-    if (position <= parentWidth - childWidth) {
-        childBlock.style.left = position + "px";
-        position += speed;
-
-        requestAnimationFrame(moveBlock);
+    if (posX + disX * speed > fixedParentWidth || posX + disX * speed < 0) {
+        [disX, disY] = [0, disX];
     }
+
+    if (posY + disY * speed > fixedParentHeight || posY + disY * speed < 0) {
+        [disX, disY] = [-dy, 0];
+    }
+
+    posX += disX * speed;
+    posY += disY * speed;
+
+    childBlock.style.left = `${posX}px`;
+    childBlock.style.top = `${posY}px`;
+
+    requestAnimationFrame(move);
 }
 
-moveBlock();
+move();
+
+//part Timer
+
+let alrdGo = null;
+
+const time = document.querySelector("#seconds")
+
+
+document.querySelector("#start").onclick = () => {
+    if (alrdGo != null) return;
+    alrdGo = setInterval (() => {
+        time.innerHTML++
+    }, 1000)
+}
+document.querySelector("#stop").onclick = () => {
+    clearInterval(alrdGo)
+    alrdGo = null
+}
+document.querySelector("#reset").onclick = () => {
+    clearInterval(alrdGo)
+    alrdGo = null
+    time.innerHTML = 0
+}
